9d7231e57d63a7f2c11f0c320f58b6d4b1ee5725
emocanmimocan <emintrhack@gmail.com>
2019-08-26 21:10:32 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+// alt + shift + f for prettier
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Card from "./components/Card";
+import Default from "./components/Default";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Card />
+      <Default />
+    </React.Fragment>
   );
 }
 
